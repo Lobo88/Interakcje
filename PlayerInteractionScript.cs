3c01169148d9737a696ff299426290f098dc90d5
Tomasz Gancarz <tomasz.gancarz.wro@gmail.com>
2021-02-08 21:08:26 +0100

    zmiana

--- PlayerInteractionScript.cs
+++ PlayerInteractionScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerInteractionScript : MonoBehaviour
 {
-    BoxScript ObjectInteract;
+    Interactable1 ObjectInteract;
     private bool CanTake=false;
 
     void Update()
